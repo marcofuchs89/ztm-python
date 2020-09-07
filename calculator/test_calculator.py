81d65f1d3655d2f42864de0f7703fc1dbacf5be4
Marco Fuchs <37122630+marcofuchs89@users.noreply.github.com>
2020-09-07 16:51:29 +0200

    Improve Calculator.py functionalities (#2)
    * Add functions
    
    Added functions for subtract, divide and toPower
    Implement exception handling for ZeroDivision error
    
    * Update unittests
    
    Updated unittest.assertions for newly implemented functions subtract(), divide(), toPower()

--- calculator/test_calculator.py
+++ calculator/test_calculator.py
@@ -7,6 +7,20 @@ class TestCalculator(TestCase):
         self.calculator = Calculator()
         self.assertEqual(self.calculator.add(3, 4), 7)
 
+    def test_subtract(self):
+        self.calculator = Calculator()
+        self.assertEqual(self.calculator.subtract(3, 4), -1)
+        self.assertEqual(self.calculator.subtract(-1, -1), 0)
+
     def test_multiply(self):
         self.calculator = Calculator()
         self.assertEqual(self.calculator.multiply(3, 4), 12)
+
+    def test_divide(self):
+        self.calculator = Calculator()
+        self.assertEqual(self.calculator.divide(12, 4), 3)
+        self.assertEqual(self.calculator.divide(12, 0), 'Division by 0 not allowed!')
+
+    def test_toPower(self):
+        self.calculator = Calculator()
+        self.assertEqual(self.calculator.toPower(8, 2), 64)
