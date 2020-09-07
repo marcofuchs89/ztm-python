614c4762ec3941f29c5e67746afe2229e41a2f6d
Marco Fuchs <marco.fuchs@live.de>
2020-09-07 16:45:44 +0200

    Add functions
    Added functions for subtract, divide and toPower
    Implement exception handling for ZeroDivision error

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -5,5 +5,17 @@ class Calculator:
     def add(self, a, b):
         return a + b
 
+    def subtract(self, a, b):
+        return a - b
+
     def multiply(self, a, b):
         return a * b
+
+    def divide(self, a, b):
+        try:
+            return a / b
+        except ZeroDivisionError:
+            return 'Division by 0 not allowed!'
+
+    def toPower(self, a, b):
+        return a ** b
